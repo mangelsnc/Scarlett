a5d7184bed1b792dae788e0fc15f59bf538f1ece
Miguel Ángel Sánchez Chordi <slayer.ma@gmail.com>
2013-10-15 07:46:31 +0200

    Delete older routes and add new bassed on Controller

--- src/app.php
+++ src/app.php
@@ -4,12 +4,9 @@ use Symfony\Component\Routing;
 
 $routes = new Routing\RouteCollection();
 
-$routes->add('hello', new Routing\Route('/hello/{name}', array(
-    'name' => 'Scarlett',
-    '_controller' => 'render_template'
-)));
-$routes->add('bye', new Routing\Route('/bye', array(
-    '_controller' => 'render_template'
+$routes->add('leap_year', new Routing\Route('/is-leap-year/{year}', array(
+    'year' => null,
+    '_controller' => 'Calendar\\Controller\\LeapYearController::indexAction'
 )));
 
 return $routes;
