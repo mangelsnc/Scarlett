2bf059c177f2f0f1590ff4de9fe1fc80b7034422
Miguel Ángel Sánchez Chordi <slayer.ma@gmail.com>
2013-11-15 00:58:44 +0100

    Now extends from Controller class and use render method to generate a response

--- src/Calendar/Controller/LeapYearController.php
+++ src/Calendar/Controller/LeapYearController.php
@@ -4,17 +4,31 @@ namespace Calendar\Controller;
  
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Scarlett\Controller\Controller;
 use Calendar\Model\LeapYear;
  
-class LeapYearController
+class LeapYearController extends Controller
 {
     public function indexAction(Request $request, $year)
     {
         $leapyear = new LeapYear();
         if ($leapyear->isLeapYear($year)) {
-            return new Response('<strong>Yep</strong>, this is a leap year!');
+            $message = 'Yep, this is a leap year!';
+        }else{
+            $message = 'Nope, this is not a leap year!';
         }
  
-        return new Response('Nope, this is not a leap year.');
+        return $this->render('leapYear', array('message' => $message));
+    }
+
+    public function primeAction(Request $request, $number)
+    {
+        if($number%$number == 0  && $number%1 == 0){
+            $message = 'Prime number';
+        }else{
+            $message = 'Not prime';
+        }
+
+        return $this->render('primeNumber', array('message' => $message));
     }
 }
