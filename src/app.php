f4c4548ded94b031ba29cf8107db3cfbd02869d3
Miguel Ángel Sánchez Chordi <slayer.ma@gmail.com>
2013-11-15 01:00:49 +0100

    Add a new test route

--- src/app.php
+++ src/app.php
@@ -9,4 +9,9 @@ $routes->add('leap_year', new Routing\Route('/is-leap-year/{year}', array(
     '_controller' => 'Calendar\\Controller\\LeapYearController::indexAction'
 )));
 
+$routes->add('prime_number', new Routing\Route('is-prime-number/{number}', array(
+    'number' => null,
+    '_controller' => 'Calendar\\Controller\\LeapYearController::primeAction'
+)));
+
 return $routes;
